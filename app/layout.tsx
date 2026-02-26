9b424c521e0e71e9500466d63ec2dac36a0accca
andrewliamc <77427633+andrewliamc@users.noreply.github.com>
2026-02-25 22:08:21 -0500

    Update head for impact

--- app/layout.tsx
+++ app/layout.tsx
@@ -152,6 +152,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+          name="impact-site-verification"
+          value="cf84e634-1160-42ca-9d9e-81a75ac4d509"
+        />
         {/* Google tag (gtag.js) */}
         <script
           async
